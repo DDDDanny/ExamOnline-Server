e0345f6af9399a45d65a7195f4ea5304b28d6969
DDDDanny <707956456@qq.com>
2019-02-22 18:25:48 +0800

    更新Model

--- apps/operatioin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OperatioinConfig(AppConfig):
-    name = 'operatioin'
--- apps/testquestion/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestquestionConfig(AppConfig):
-    name = 'testquestion'
