cec15cdc66c62368b9641ac521747b8d139f5248
DDDDanny <707956456@qq.com>
2023-12-29 18:00:22 +0800

    feat：增加用户管理路由，并进行注册

--- src/apps/UserManagement/views.py
+++ src/apps/UserManagement/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from src.utils.logger_utils import log_request, log_response
+from rest_framework.decorators import action
+from src.utils.response_utils import ResponseCode, api_response
+
+
+@action(detail=False, methods=['GET'])
+def index(request):
+    log_request(json.dumps({ 'userName': '007', 'password': '123456' }))
+    data = api_response(ResponseCode.SUCCESS, '获取成功', data={ 'userName': '007', 'password': '123456' })
+    log_response(data.content.decode('unicode_escape'))
+    return data
