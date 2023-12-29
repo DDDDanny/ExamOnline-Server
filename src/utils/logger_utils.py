736deab9d927e4bcc890267d39a9b4ef333b5531
DDDDanny <707956456@qq.com>
2023-12-29 17:59:54 +0800

    feat：增加日志公共方法，方便编写请求&返回数据

---
+++ src/utils/logger_utils.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/29 17:42:27
+# @Author  : DannyDong
+# @File    : logger_utils.py
+# @Describe: 日志公共方法
+
+import logging
+
+
+def log_request(request_body: str):
+    """
+    记录请求体的日志。
+    Args:
+        request_body (str): 请求体数据。
+    """
+    logger = logging.getLogger('django')
+    log_message = f'Request Body: {request_body}'
+    logger.info(log_message)
+
+
+def log_response(response_body: str):
+    """
+    记录响应体的日志。
+    Args:
+        response_body (str): 响应体数据。
+    """
+    logger = logging.getLogger('django')
+    log_message = f'Response Body: {response_body}'
+    logger.info(log_message)
+
+
+if __name__ == '__main__':
+    pass
