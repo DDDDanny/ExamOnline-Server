3a8092e18bbe6f625ad7d6fa0d2b390afb8f376d
DDDDanny <707956456@qq.com>
2024-01-04 16:06:04 +0800

    feat：新增用户信息序列化

---
+++ src/apps/UserManagement/serializers.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/01/04 10:26:19
+# @Author  : DannyDong
+# @File    : serializers.py
+# @Describe: 用户应用-序列化 
+
+from .models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # 指定在序列化器中包含的字段
+        fields = [
+            'username', 'password', 'role', 'phone', 
+            'email', 'is_active', 'created_at', 'updated_at'
+        ]
+        # 将 'password' 字段设置为只写，以在响应中隐藏它
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+if __name__ == '__main__':
+    pass
