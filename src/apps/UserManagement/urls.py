e54f2ca2331ced577f0e8b8ea02dcc3b291c5b3f
DDDDanny <707956456@qq.com>
2024-01-04 17:08:25 +0800

    feat：调整创建用户接口地址

--- src/apps/UserManagement/urls.py
+++ src/apps/UserManagement/urls.py
@@ -9,7 +9,7 @@ from django.urls import path
 from .views import CreateUserView
 
 urlpatterns = [
-    path('createUser/', CreateUserView.as_view(), name='create_user'),
+    path('createUser', CreateUserView.as_view(), name='create_user'),
 ]
 
 
