8c90d3cb3d1bae379c98fe3ef7cdf31f74294bc5
DDDDanny <707956456@qq.com>
2024-01-04 16:06:19 +0800

    feat：新增创建用户信息接口

--- src/apps/UserManagement/urls.py
+++ src/apps/UserManagement/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import CreateUserView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('createUser/', CreateUserView.as_view(), name='create_user'),
 ]
--- src/apps/UserManagement/views.py
+++ src/apps/UserManagement/views.py
@@ -1,13 +1,30 @@
-import json
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .serializers import UserSerializer
 from src.utils.logger_utils import log_request, log_response
-from rest_framework.decorators import action
 from src.utils.response_utils import ResponseCode, api_response
 
 
-@action(detail=False, methods=['GET'])
-def index(request):
-    log_request(json.dumps({ 'userName': '007', 'password': '123456' }))
-    data = api_response(ResponseCode.SUCCESS, '获取成功', data={ 'userName': '007', 'password': '123456' })
-    log_response(data.content.decode('unicode_escape'))
-    return data
+class CreateUserView(APIView):
+    # 创建用户信息
+    def post(self, request):
+        # 使用请求数据创建 UserSerializer 的实例
+        serializer = UserSerializer(data=request.data)
+        log_request(request.data)
+        # 检查数据是否根据序列化器的规则有效
+        if serializer.is_valid():
+            # 保存验证过的数据以创建新的 User 实例
+            serializer.save()
+            # 返回成功响应，包含序列化后的数据和 HTTP 201 Created 状态
+            data = Response(serializer.data)
+            log_response(data.data)
+            return api_response(ResponseCode.SUCCESS, '创建成功', data.data)
+        else:
+            # 返回错误响应，包含验证错误和 HTTP 400 Bad Request 状态
+            log_response(serializer.errors)
+            return api_response(ResponseCode.BAD_REQUEST, '创建失败', serializer.errors)
+
+
+if __name__ == '__main__':
+    pass
