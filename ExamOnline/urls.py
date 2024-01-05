2290a1eddff7209e9efc890f5011af8ed85266bc
DDDDanny <707956456@qq.com>
2024-01-05 17:58:11 +0800

    feat：用户应用整体的接口地址

--- ExamOnline/urls.py
+++ ExamOnline/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("UserManagement/", include("src.apps.UserManagement.urls")),
     path('admin/', admin.site.urls),
+    path("", include("src.apps.UserManagement.urls")),
 ]
