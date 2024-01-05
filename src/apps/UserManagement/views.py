77b3cb2a0702315ceb5256eb8631e6cc99d54e18
DDDDanny <707956456@qq.com>
2024-01-05 15:24:19 +0800

    perf：清除接口方法中的日志打印逻辑

--- src/apps/UserManagement/views.py
+++ src/apps/UserManagement/views.py
@@ -19,18 +19,15 @@ class CreateUserView(APIView):
     def post(self, request):
         # 使用请求数据创建 UserSerializer 的实例
         serializer = UserSerializer(data=request.data)
-        log_request(request.data)
         # 检查数据是否根据序列化器的规则有效
         if serializer.is_valid():
             # 保存验证过的数据以创建新的 User 实例
             serializer.save()
             # 返回成功响应，包含序列化后的数据和 HTTP 201 Created 状态
             data = Response(serializer.data)
-            log_response(data.data)
             return api_response(ResponseCode.SUCCESS, '创建成功', data.data)
         else:
             # 返回错误响应，包含验证错误和 HTTP 400 Bad Request 状态
-            log_response(serializer.errors)
             return api_response(ResponseCode.BAD_REQUEST, '创建失败', serializer.errors)
 
 
@@ -54,7 +51,6 @@ class ListUsersView(ListAPIView):
         serializer = UserSerializer(queryset, many=True)
         # 返回序列化后的数据
         data = Response(serializer.data)
-        log_response(data.data)
         return api_response(ResponseCode.SUCCESS, '查询成功', data.data)
 
 
---
+++ src/middleware/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/01/05 14:36:54
+# @Author  : DannyDong
+# @File    : __init__.py
+# @Describe: None 
