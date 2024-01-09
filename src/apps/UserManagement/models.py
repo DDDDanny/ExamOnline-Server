d93643a3dd1b03907b761fd20b0686a42daa0abd
DDDDanny <707956456@qq.com>
2024-01-09 11:28:36 +0800

    feat: 重构User模型，新增了Student和Teacher模型

--- src/apps/UserManagement/models.py
+++ src/apps/UserManagement/models.py
@@ -9,19 +9,28 @@ from django.db import models
 from django.utils import timezone
 
 
-# 用户模型
+# 用户模型（抽象模型）
 class User(models.Model):
     class Meta:
-        db_table = 'user'
+        abstract = True
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # 用户名
     username = models.CharField(max_length=30, unique=True, help_text='用户名')
     # 密码
     password = models.CharField(max_length=30, help_text='用户密码')
+    # 用户姓名
+    name = models.CharField(max_length=30, help_text='用户姓名')
     # 角色
     ROLE_CHOICES = [('student', 'Student'),('teacher', 'Teacher'),('admin', 'Admin'),]
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student', help_text='角色名')
+    # 性别
+    GENDER_CHOICES = [('male', 'Male'),('female', 'Female')]
+    gender = models.CharField(max_length=10,  choices=GENDER_CHOICES, default='male', help_text='性别')
+    # 是否活跃
+    is_active = models.BooleanField(default=True, help_text='是否激活')
+    # 是否删除
+    is_deleted = models.BooleanField(default=False, help_text='是否删除')
     # 电话
     phone = models.CharField(max_length=20, null=True, help_text='电话号码')
     # Email
@@ -30,17 +39,39 @@ class User(models.Model):
     created_at = models.DateTimeField(default=timezone.now, help_text='创建时间')
     # 更新时间
     updated_at = models.DateTimeField(auto_now=True, help_text='更新时间')
-    # 是否活跃
-    is_active = models.BooleanField(default=True, help_text='是否激活')
-    # 是否删除
-    is_deleted = models.BooleanField(default=False, help_text='是否删除')
 
+
+# 学生用户模型（实例模型）
+class Student(User):
+    class Meta:
+        db_table = 'student'
+    
+    # 学号
+    student_id = models.CharField(max_length=30, unique=True, help_text='学号')
+    # 年级
+    grade = models.CharField(max_length=30, null=True, help_text='年级')
+    
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'role', 'is_active', 'is_deleted']
+    REQUIRED_FIELDS = ['student_id', 'username', 'name', 'gender', 'role', 'is_active', 'is_deleted']
 
     def __str__(self):
-        return self.username
+        return 'Student: ' + self.username
+
+
+# 教师用户模型（实例模型）
+class Teacher(User):
+    class Meta:
+        db_table = 'teacher'
     
+    # 教师编号
+    teacher_id = models.CharField(max_length=30, unique=True, help_text='学号')
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['teacher_id', 'username', 'name', 'gender', 'role', 'is_active', 'is_deleted']
+     
+    def __str__(self):
+        return 'Teacher: ' + self.username
+
 
 if __name__ == '__main__':
     pass
