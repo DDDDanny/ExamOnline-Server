b60880cfbcc6b3435a4d55bb7b26972ea63e5493
DDDDanny <707956456@qq.com>
2024-01-11 11:43:26 +0800

    feat: 新增自定义获取&刷新Token，重写for_user方法

--- src/middleware/authentication.py
+++ src/middleware/authentication.py
@@ -5,15 +5,31 @@
 # @Describe: 自定义JWT验证 
 
 
-from src.apps.UserManagement.models import Student
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from src.apps.UserManagement.models import Student, Teacher
+
+# 自定义获取&刷新Token
+class CustomRefreshToken(RefreshToken):
+    @classmethod
+    def for_user(cls, user):
+        token = super().for_user(user)
+        # 在这里添加额外的信息到 token 中，比如 user_type
+        token['user_type'] = user.role
+        return token
+
 
 # 自定义JWT验证（由于我们的ID是UUID，默认的只支持Number）
 class CustomJWTAuthentication(JWTAuthentication):
     def get_user(self, validated_token):
         user_id = validated_token.get('user_id')
+        user_type = validated_token.get('user_type')
         try:
-            return Student.objects.get(id=user_id)
+            if user_type == 'student':
+                return Student.objects.get(id=user_id)
+            else:
+                return Teacher.objects.get(id=user_id)
         except Exception:
             return None
 
