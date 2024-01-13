97309becd0d655549412b35c3a44aa335235baeb
DDDDanny <707956456@qq.com>
2024-01-13 23:23:28 +0800

    feat：配置获取学生和教师的详情路由

--- src/apps/UserManagement/urls.py
+++ src/apps/UserManagement/urls.py
@@ -11,7 +11,9 @@ from .views import StudentLoginView, TeacherLoginView
 
 urlpatterns = [
     path('student', StudentUserView.as_view(), name='StudentOpts'),
+    path('student/<str:user_id>', StudentUserView.as_view(), name='StudentDetail'),
     path('teacher', TeacherUserView.as_view(), name='TeacherOpts'),
+    path('teacher/<str:user_id>', TeacherUserView.as_view(), name='TeacherDetail'),
     path('teacherLogin', TeacherLoginView.as_view(), name='teacherLogin'),
     path('studentLogin', StudentLoginView.as_view(), name='studentLogin'),
 ]
