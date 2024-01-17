2941510ac8ab4030dce7601fb453abc479be6aed
DDDDanny <707956456@qq.com>
2024-01-17 22:45:12 +0800

    feat: 初始化Question视图

--- src/apps/QuestionManagement/views.py
+++ src/apps/QuestionManagement/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+# @Time    : 2024/01/17 22:42:58
+# @Author  : DannyDong
+# @File    : views.py
+# @Describe: Question相关视图
 
-# Create your views here.
+from .models import Questions
+from src.utils.response_utils import ResponseCode, api_response
+
+
+if __name__ == '__main__':
+    pass
