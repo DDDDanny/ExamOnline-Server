9e4f4a9e516609c234f5a4159854f6de30fa0484
DDDDanny <707956456@qq.com>
2024-01-18 22:42:16 +0800

    perf：优化代码

--- src/apps/QuestionManagement/apps.py
+++ src/apps/QuestionManagement/apps.py
@@ -13,3 +13,7 @@ class QuestionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     # 这个属性指定了应用程序的包路径。它告诉 Django 在应用程序启动时使用哪个配置类
     name = 'src.apps.QuestionManagement'
+
+
+if __name__ == '__main__':
+    pass
