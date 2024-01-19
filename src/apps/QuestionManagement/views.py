11b6d6b0c42330d7c0da65480e0f7e18a4b56e7c
DDDDanny <707956456@qq.com>
2024-01-19 17:02:46 +0800

    feat：初始化试题基础操作视图

--- src/apps/QuestionManagement/views.py
+++ src/apps/QuestionManagement/views.py
@@ -4,8 +4,31 @@
 # @File    : views.py
 # @Describe: Question相关视图
 
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Questions
+from .serializers import QuestionSerializer
 from src.utils.response_utils import ResponseCode, api_response
+
+
+class QuestionBaseView(APIView):
+    
+    # 创建试题信息
+    def post(self, request):
+        pass
+    
+    # 修改试题信息
+    def put(self, request):
+        pass
+    
+    # 获取试题信息
+    def get(self, request, *args, **kwargs):
+        pass
+    
+    # 删除试题信息
+    def delete(self, request):
+        pass 
 
 
 if __name__ == '__main__':
