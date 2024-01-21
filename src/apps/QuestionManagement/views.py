e76d50d65f8585fc271fae2d42fa6723d5899e83
DDDDanny <707956456@qq.com>
2024-01-21 19:51:44 +0800

    feat: 新增编辑试题逻辑处理

--- src/apps/QuestionManagement/views.py
+++ src/apps/QuestionManagement/views.py
@@ -26,16 +26,31 @@ class QuestionBaseView(APIView):
             return api_response(ResponseCode.BAD_REQUEST, '创建失败', serializer.errors)      
     
     # 修改试题信息
-    def put(self, request):
-        pass
+    def put(self, request, **kwargs):
+        try:
+            # 获取需要编辑的试题实例
+            question_instance = Questions.objects.get(id=kwargs['id'])
+        except Questions.DoesNotExist:
+            return api_response(ResponseCode.NOT_FOUND, '编辑失败!试题不存在，无法进行修改！')
+        serializer = QuestionSerializer(question_instance, request.data)
+        # 检查数据是否根据序列化器的规则有效
+        if serializer.is_valid():
+            # 保存验证过的数据以更新现有的 Question 实例
+            serializer.save()
+            # 返回成功响应，包含序列化后的数据和 HTTP 200 OK 状态
+            data = Response(serializer.data)
+            return api_response(ResponseCode.SUCCESS, '编辑成功', data.data)
+        else:
+            # 返回错误响应，包含验证错误和 HTTP 400 Bad Request 状态
+            return api_response(ResponseCode.BAD_REQUEST, '编辑失败! 存在校验失败的字段！', serializer.error_messages)
     
+    # 删除试题信息
+    def delete(self, _, **kwargs):
+        pass
+
     # 获取试题信息
     def get(self, request, *args, **kwargs):
         pass
-    
-    # 删除试题信息
-    def delete(self, request):
-        pass 
 
 
 if __name__ == '__main__':
