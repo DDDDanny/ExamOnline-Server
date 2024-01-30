1c27ac73cc613ba50eb46e52918518e0c7146940
DDDDanny <707956456@qq.com>
2024-01-30 11:15:55 +0800

    feat：配置查询某人收藏试题列表路由

--- src/apps/QuestionManagement/urls.py
+++ src/apps/QuestionManagement/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('question', QuestionBaseView.as_view(), name='QuestionsOpts'),
     path('question/<str:id>', QuestionBaseView.as_view(), name='QuestionsOpts'),
     path('q_favorite', QuestionFavoriteView.as_view(), name='QuestionsFavoriteOpts'),
+    path('q_favorite/<str:id>', QuestionFavoriteView.as_view(), name='QuestionsFavoriteOpts'),
 ]
 
 
