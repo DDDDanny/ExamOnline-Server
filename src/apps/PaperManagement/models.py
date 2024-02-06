5e131104a5179e3caa6be013b841182f560f05a8
DDDDanny <707956456@qq.com>
2024-02-06 10:17:44 +0800

    feat: 新增Paper Model

--- src/apps/PaperManagement/models.py
+++ src/apps/PaperManagement/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+# @Time    : 2024/02/06 10:07:03
+# @Author  : DannyDong
+# @File    : models.py
+# @Describe: Paper应用模型 
 
-# Create your models here.
+import uuid
+
+from django.db import models
+from django.utils import timezone
+
+
+class Paper(models.Model):
+    class Meta:
+        db_table = 'paper'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # 试卷名称
+    title = models.CharField(max_length=100, help_text='试卷标题')
+    # 试卷描述
+    description = models.TextField(blank=True, null=True, help_text='试卷描述')
+    # 答题建议时长
+    duration_minutes = models.PositiveIntegerField(default=60, help_text='答题建议时长')
+    # 总分数
+    total_marks = models.PositiveIntegerField(default=100, help_text='总分数')
+    # 是否发布
+    is_published = models.BooleanField(default=False, help_text='是否发布')
+    # 发布时间
+    publish_date = models.DateTimeField(null=True, blank=True, verbose_name='发布时间')
+    # 创建人
+    created_user = models.CharField(max_length=255, help_text='创建人')
+    # 创建时间
+    created_at = models.DateTimeField(default=timezone.now, help_text='创建时间')
+    # 更新人
+    updated_user = models.CharField(max_length=255, null=True, help_text='更新人')
+    # 更新时间
+    updated_at = models.DateTimeField(auto_now=True, null=True, help_text='更新时间')
+
+
+if __name__ == '__main__':
+    pass
