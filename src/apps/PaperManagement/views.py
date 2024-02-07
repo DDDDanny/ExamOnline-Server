22a44908f460607c746ef22215728b83ba64e778
DDDDanny <707956456@qq.com>
2024-02-07 10:10:25 +0800

    feat: 初始化Paper相关视图

--- src/apps/PaperManagement/views.py
+++ src/apps/PaperManagement/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+# @Time    : 2024/02/07 09:58:21
+# @Author  : DannyDong
+# @File    : views.py
+# @Describe: Paper相关视图 
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Paper
+from .serializers import PaperSerializer
+from src.utils.response_utils import ResponseCode, api_response
+
+
+class PaperBaseView(APIView):
+    pass
+
+
+if __name__ == '__main__':
+    pass
