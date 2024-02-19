57aacbf552b8edcd34a3fbb5ce4fed4e6de44a1f
DDDDanny <707956456@qq.com>
2024-02-19 16:07:48 +0800

    feat: 新增公共的 Serializer Mixin，用于获取用户详细信息

--- src/apps/PaperManagement/serializers.py
+++ src/apps/PaperManagement/serializers.py
@@ -6,21 +6,54 @@
 
 from rest_framework import serializers
 
+from src.apps.UserManagement.models import Teacher
 from .models import Paper, PaperModule, PaperQuestions
 
+# 创建一个公共的 Serializer Mixin
+class UserInfoMixin:
+    def get_user_info(self, user_id):
+        """get_user_info 
+        根据给定的用户ID查询数据库获取用户信息，并返回包含用户信息的字典。
+        Args:
+            user_id (str): 用户的ID
+        Returns:
+            如果找到用户，则返回包含用户信息的字典，否则返回None。
+        """
+        teacher_instance = Teacher.objects.filter(id=user_id).first()
+        if teacher_instance:
+            return {
+                'id': teacher_instance.id,
+                'name': teacher_instance.name, 
+                'username': teacher_instance.username,
+                # 可以再加需要的数据
+            }
+        else:
+            return None
 
-class PaperSerializer(serializers.ModelSerializer):
+
+class PaperSerializer(serializers.ModelSerializer, UserInfoMixin):
+    created_user_info = serializers.SerializerMethodField()
+    updated_user_info = serializers.SerializerMethodField()
+    
     class Meta:
         model = Paper
         # 指定在序列号中包含的字段
         fields = '__all__'
         # 添加额外的字段
-        # extra_field = ['created_user_info', 'updated_user_info']
+        extra_field = ['created_user_info', 'updated_user_info']
         # 格式化日期时间
         extra_kwargs = {
             'created_at': { 'format': '%Y-%m-%d %H:%M:%S' },
             'updated_at': { 'format': '%Y-%m-%d %H:%M:%S' },
         }
+    
+    # 获取创建人信息
+    def get_created_user_info(self, obj):
+        return self.get_user_info(obj.created_user)
+    
+    # 获取更新人信息
+    def get_updated_user_info(self, obj):
+        return self.get_user_info(obj.updated_user)
 
 
 class PaperModuleSerializer(serializers.ModelSerializer):
