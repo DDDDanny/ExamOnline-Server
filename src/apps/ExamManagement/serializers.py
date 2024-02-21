6ec31464c270accd5781997fc73f448366e6317a
DDDDanny <707956456@qq.com>
2024-02-21 14:57:36 +0800

    feat: 新增ExamSerializers

--- src/apps/ExamManagement/serializers.py
+++ src/apps/ExamManagement/serializers.py
@@ -4,6 +4,24 @@
 # @File    : serializers.py
 # @Describe: Exam应用-序列化
 
+from rest_framework import serializers
+
+from .models import Exam
+
+
+class ExamSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Exam
+        fields = '__all__'
+        # 添加额外的字段
+        # extra_field = ['created_user_info', 'updated_user_info']
+        # 格式化日期时间
+        extra_kwargs = {
+            'created_at': { 'format': '%Y-%m-%d %H:%M:%S' },
+            'updated_at': { 'format': '%Y-%m-%d %H:%M:%S' },
+        }
+
 
 if __name__ == '__main__':
     pass
