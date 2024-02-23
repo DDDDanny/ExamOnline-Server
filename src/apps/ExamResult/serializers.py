c751d57c2675c8168a8fb205ef2fa33e768b52ab
DDDDanny <707956456@qq.com>
2024-02-23 13:11:54 +0800

    feat: 初始化 ExamResult应用-序列化

---
+++ src/apps/ExamResult/serializers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/02/23 13:09:19
+# @Author  : DannyDong
+# @File    : serializers.py
+# @Describe: ExamResult应用-序列化
+
+from rest_framework import serializers
+
+
+if __name__ == '__main__':
+    pass
