9f4709f5812ef6b7342b0483da259e14afdb45ab
DDDDanny <707956456@qq.com>
2024-02-23 13:12:35 +0800

    feat：初始化：ExamResult应用URL

---
+++ src/apps/ExamResult/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/02/23 13:09:25
+# @Author  : DannyDong
+# @File    : urls.py
+# @Describe: ExamResult应用URL
+
+from django.urls import path
+
+
+urlpatterns = [
+    
+]
