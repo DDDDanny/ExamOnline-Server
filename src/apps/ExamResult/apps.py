501b6ec6eee915d53ca700575ea525121ccfd9da
DDDDanny <707956456@qq.com>
2024-02-23 13:14:36 +0800

    feat: 配置ExamResult应用apps

--- src/apps/ExamResult/apps.py
+++ src/apps/ExamResult/apps.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/02/23 13:13:44
+# @Author  : DannyDong
+# @File    : apps.py
+# @Describe: ExamResult应用apps 
+
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class ExamResultConfig(AppConfig):
+    # 属性指定了应用程序的默认主键字段类型
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Users'
+    # 这个属性指定了应用程序的包路径。它告诉 Django 在应用程序启动时使用哪个配置类
+    name = 'src.apps.ExamResult'
