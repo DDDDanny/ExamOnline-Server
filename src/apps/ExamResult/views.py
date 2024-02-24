cc4b78bd7a58feec64f06d269c96a7022d6ffbc6
DDDDanny <707956456@qq.com>
2024-02-24 20:08:06 +0800

    feat：初始化ExamResultBaseView

--- src/apps/ExamResult/views.py
+++ src/apps/ExamResult/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+# @Time    : 2024/02/24 20:05:21
+# @Author  : DannyDong
+# @File    : views.py
+# @Describe: ExamResult应用视图层
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import ExamResult
+from .serializers import ExamResultSerializer
+from src.utils.response_utils import ResponseCode, api_response
+
+
+class ExamResultBaseView(APIView):
+    pass
+
+
+if __name__ == '__main__':
+    pass
