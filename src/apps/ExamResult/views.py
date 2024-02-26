0933f823ebce6c4e4a186e5ed1eaeae1a0918105
DDDDanny <707956456@qq.com>
2024-02-26 12:34:27 +0800

    feat：新增删除考试结果信息（物理删除）逻辑及接口

--- src/apps/ExamResult/views.py
+++ src/apps/ExamResult/views.py
@@ -28,6 +28,23 @@ class ExamResultBaseView(APIView):
         else:
             return api_response(ResponseCode.BAD_REQUEST, '创建失败', serializer.errors)
 
+    def delete(self, _, **kwargs):
+        """delete 删除考试结果信息（物理删除）
+        Args:
+            _ (-): 缺省参数
+            id (Object): 考试ID
+        """
+        try:
+            exam_instance = ExamResult.objects.get(id=kwargs['id'])
+        except ExamResult.DoesNotExist:
+            return api_response(ResponseCode.NOT_FOUND, '考试结果不存在，删除失败！')
+        if exam_instance.start_time is not None:
+            return api_response(ResponseCode.BAD_REQUEST, '该学生已经参加考试，无法删除！')
+        else:
+            exam_instance.delete()
+            # 返回成功响应
+            return api_response(ResponseCode.SUCCESS, '删除成功！')
+
 
 class ExamResultDetailBaseView(APIView):
     pass
