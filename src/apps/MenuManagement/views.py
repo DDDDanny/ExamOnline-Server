51b32cf0f0a46812a6480a666cdbebc5bfecc65f
DDDDanny <707956456@qq.com>
2024-03-08 18:15:10 +0800

    feat：新增创建菜单和获取菜单列表接口

--- src/apps/MenuManagement/views.py
+++ src/apps/MenuManagement/views.py
@@ -4,6 +4,51 @@
 # @File    : views.py
 # @Describe: Menu应用视图层
 
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Menu
+from .serializers import MenuSerializer
+from src.utils.response_utils import ResponseCode, api_response
+
+
+class MenuBaseView(APIView):
+    
+    def post(self, request):
+        """post 创建菜单
+        Args:
+            request (Object): 新增菜单必填参数
+        """
+        serializer = MenuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = Response(serializer.data)
+            return api_response(ResponseCode.SUCCESS, '创建成功', data.data)
+        else:
+            return api_response(ResponseCode.BAD_REQUEST, '创建失败', serializer.errors)
+    
+    def get(self, request):
+        """get 获取菜单信息
+        Args:
+            request (Object): 请求参数
+        """
+        is_show_value = request.query_params.get('is_show', None)
+        # 执行查询
+        if is_show_value is None or is_show_value == 'Admin':
+            queryset = Menu.objects.filter().order_by('order')
+        else:
+            if is_show_value == 'Teacher' or is_show_value == 'Student':
+                queryset = Menu.objects.filter(Q(is_show=is_show_value) | Q(is_show='ALL')).order_by('order')
+            else:
+                return api_response(ResponseCode.BAD_REQUEST, '获取菜单失败！角色错误！')
+        # 序列化试题数据
+        serializer = MenuSerializer(queryset, many=True)
+        # 返回序列化后的数据
+        data = Response(serializer.data)
+        resp = { 'total': len(data.data), 'data': data.data }
+        return api_response(ResponseCode.SUCCESS, '查询成功', resp)
+
+
+if __name__ == '__main__':
+    pass
