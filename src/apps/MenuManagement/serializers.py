df886990af0f3429ee65eea537b7511a247aa2b1
DDDDanny <707956456@qq.com>
2024-03-08 17:03:04 +0800

    feat：新增menu序列化

--- src/apps/MenuManagement/serializers.py
+++ src/apps/MenuManagement/serializers.py
@@ -6,6 +6,13 @@
 
 from rest_framework import serializers
 
+from .models import Menu
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
 
 if __name__ == '__main__':
     pass
