4b4d3187369de5a4cec7cd1ad52c95e61f6fa1a4
DDDDanny <707956456@qq.com>
2024-03-08 16:57:18 +0800

    feat：增加menu，并进行数据迁移

--- src/apps/MenuManagement/models.py
+++ src/apps/MenuManagement/models.py
@@ -4,6 +4,23 @@
 # @File    : models.py
 # @Describe: Menu应用模型
 
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    class Meta:
+        db_table = 'menu'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # 菜单Code
+    code = models.CharField(max_length=255, help_text='菜单Code')
+    # 菜单名称
+    name = models.CharField(max_length=255, help_text='菜单名称')
+    # 根据角色显示
+    is_show = models.CharField(default='ALL', max_length=50, help_text='根据角色显示')
+    # 菜单Path
+    path = models.CharField(max_length=255, help_text='path')
+    # 菜单排序
+    order = models.PositiveIntegerField(default=0, help_text='菜单排序')
