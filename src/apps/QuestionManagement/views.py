8f6f8115c3046ba2c44105ea4eeb840b12d42b9d
DDDDanny <707956456@qq.com>
2024-03-20 13:29:55 +0800

    feat：调整编辑试题接口，删除不需要的参数

--- src/apps/QuestionManagement/views.py
+++ src/apps/QuestionManagement/views.py
@@ -40,6 +40,9 @@ class QuestionBaseView(APIView):
         try:
             # 获取需要编辑的试题实例
             question_instance = Questions.objects.get(id=kwargs['id'])
+            del request.data['updatedAt']
+            del request.data['createdAt']
+            del request.data['id']
         except Questions.DoesNotExist:
             return api_response(ResponseCode.NOT_FOUND, '编辑失败!试题不存在，无法进行修改！')
         serializer = QuestionSerializer(question_instance, request.data)
