ae81726447038cd13a4fd4dfb7b87ab73a2eca71
DDDDanny <707956456@qq.com>
2024-03-24 19:07:57 +0800

    feat：调整根据收藏者ID获取收藏的试题列表逻辑，增加分页

--- src/apps/QuestionManagement/views.py
+++ src/apps/QuestionManagement/views.py
@@ -181,18 +181,22 @@ class QuestionFavoriteView(APIView):
             else:
                 return api_response(ResponseCode.BAD_REQUEST, '取消收藏失败！没有找到相关的收藏记录！')
 
-    def get(self, _, **kwargs):
+    def get(self, request, **kwargs):
         """get 根据收藏者ID获取收藏的试题列表
         Args:
             _ (any): 缺省参数
             id (str): 收藏者ID
         """
         queryset = QuestionsFavorite.objects.filter(collector=kwargs['id'])
-        # 序列化试题数据
-        serializer = QuestionFavoriteSerializer(queryset, many=True)
-        # 返回序列化后的数据
-        data = Response(serializer.data)
-        resp = {'total': len(data.data), 'data': data.data}
+        # 实例化分页器并配置参数
+        paginator = PageNumberPagination()
+        paginator.page_size = int(request.query_params.get('pageSize', 50))
+        paginator.page_query_param = 'currentPage'
+        # 进行分页处理
+        paginated_queryset = paginator.paginate_queryset(queryset, request)
+        # 序列化分页后的试题数据
+        serializer = QuestionFavoriteSerializer(paginated_queryset, many=True)
+        resp = {'total': len(queryset), 'data': serializer.data}
         return api_response(ResponseCode.SUCCESS, '查询成功', resp)
 
 
