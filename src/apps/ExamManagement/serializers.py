f7dd733a030ca0dc15c864408227373f61b40c10
DDDDanny <707956456@qq.com>
2024-04-19 15:05:34 +0800

    feat：Exam在做序列化时，新增试卷信息的返回

--- src/apps/ExamManagement/serializers.py
+++ src/apps/ExamManagement/serializers.py
@@ -7,15 +7,17 @@
 from rest_framework import serializers
 
 from .models import Exam
+from ..PaperManagement.models import Paper
 
 
 class ExamSerializer(serializers.ModelSerializer):
+    paper_info = serializers.SerializerMethodField()
     
     class Meta:
         model = Exam
         fields = '__all__'
         # 添加额外的字段
-        # extra_field = ['created_user_info', 'updated_user_info']
+        extra_field = ['paper_info']
         # 格式化日期时间
         extra_kwargs = {
             'created_at': { 'format': '%Y-%m-%d %H:%M:%S' },
@@ -24,6 +26,18 @@ class ExamSerializer(serializers.ModelSerializer):
             'start_time': { 'format': '%Y-%m-%d %H:%M:%S' },
             'end_time': { 'format': '%Y-%m-%d %H:%M:%S' },
         }
+    
+    # 获取试卷信息
+    def get_paper_info(self, obj):
+        paper_instance = Paper.objects.filter(id=obj.paper_id).first()
+        if paper_instance:
+            return {
+                'id': paper_instance.id,
+                'title': paper_instance.title, 
+                # 可以再加需要的数据
+            }
+        else:
+            return None
 
 
 if __name__ == '__main__':
