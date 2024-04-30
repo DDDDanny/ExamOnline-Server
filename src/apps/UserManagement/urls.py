d5264e509250d8cd95630ba37a5090d0bb30af3c
DDDDanny <707956456@qq.com>
2024-04-30 10:16:48 +0800

    feat：修改用户动态路由

--- src/apps/UserManagement/urls.py
+++ src/apps/UserManagement/urls.py
@@ -13,9 +13,9 @@ from .views import StudentChangePasswordView, TeacherChangePasswordView
 
 urlpatterns = [
     path('student', StudentUserView.as_view(), name='StudentOpts'),
-    path('student/<str:user_id>', StudentUserView.as_view(), name='StudentDetail'),
+    path('student/<str:id>', StudentUserView.as_view(), name='StudentDetail'),
     path('teacher', TeacherUserView.as_view(), name='TeacherOpts'),
-    path('teacher/<str:user_id>', TeacherUserView.as_view(), name='TeacherDetail'),
+    path('teacher/<str:id>', TeacherUserView.as_view(), name='TeacherDetail'),
     path('teacherLogin', TeacherLoginView.as_view(), name='TeacherLogin'),
     path('studentLogin', StudentLoginView.as_view(), name='StudentLogin'),
     path('studentBatchActivation', StudentBatchActivationView.as_view(), name='StudentBatchActivation'),
