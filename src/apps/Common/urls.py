fe94a096a207e119ff5ba4e73fbc0cb7fd23253f
DDDDanny <707956456@qq.com>
2024-10-22 19:01:43 +0800

    feat：配置成绩单下载路由

--- src/apps/Common/urls.py
+++ src/apps/Common/urls.py
@@ -7,9 +7,11 @@
 from django.urls import path
 
 from .views import DownloadFileView
+from .views import DownloadResultFileView
 
 urlpatterns = [
     path('download/<str:filename>', DownloadFileView.as_view(), name='download_file'),
+    path('downloadResult/<str:filename>', DownloadResultFileView.as_view(), name='download_result_file'),
 ]
 
 
